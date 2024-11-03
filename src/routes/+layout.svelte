de38c062da222fbda96134035e58272d8715a308
Wahyu Haryanto <way3haryanto@gmail.com>
2024-11-03 09:48:29 +0700

    fix deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Sefloo</title>
+</svelte:head>
+
 <slot />
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs18.x'
+		}),
 		alias: {
 			$lib: './src/lib'
 		}
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
