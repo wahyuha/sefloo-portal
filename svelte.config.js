7a509be14f4d4ae653b419dc6cc2b915c8eeca79
Wahyu Haryanto <way3haryanto@gmail.com>
2024-11-03 10:02:35 +0700

    remove runtime 18x

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: 'nodejs18.x'
-		}),
+		adapter: adapter(),
 		alias: {
 			$lib: './src/lib'
 		}
